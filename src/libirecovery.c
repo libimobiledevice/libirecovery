010b3b08c87bb3586e7b31668c62b8848a1bb759
Nikias Bassen <nikias@gmx.li>
2023-12-08 11:14:04 +0100

    win32: Only try to set alt interface for interface 1
    This is the same as we do for the other platforms, so I assume this is OK

--- src/libirecovery.c
+++ src/libirecovery.c
@@ -2064,8 +2064,10 @@ irecv_error_t irecv_usb_set_interface(irecv_client_t client, int usb_interface, 
 	}
 #endif
 #else
-	if (irecv_usb_control_transfer(client, 0, 0x0B, usb_alt_interface, usb_interface, NULL, 0, USB_TIMEOUT) < 0) {
-		return IRECV_E_USB_INTERFACE;
+	if (usb_interface == 1) {
+		if (irecv_usb_control_transfer(client, 0, 0x0B, usb_alt_interface, usb_interface, NULL, 0, USB_TIMEOUT) < 0) {
+			return IRECV_E_USB_INTERFACE;
+		}
 	}
 #endif
 	client->usb_interface = usb_interface;
